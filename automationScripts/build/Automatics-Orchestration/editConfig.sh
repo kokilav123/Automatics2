a99446fbcfc48a01f99407c9c270fecc982e8cfd
sshere654 <Shameem_Shereef@comcast.com>
2022-09-07 12:07:54 +0530

    AUTOMATICS-175 : Automatics Jenkins server and Job creation automation
    Reason for change:  Automatics Jenkins server and Job creation automation
    Test Procedure: Not applicable.
    Risks: None
    
    Change-Id: Ied552daaddb54d2c0e6f9f85b8b1986b8ebd846a
    Signed-off-by: Shameem M Shereef <shameemm.s@tataelxsi.co.in>

---
+++ automationScripts/build/Automatics-Orchestration/editConfig.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright (c) 2022 Comcast Cable Communications Management, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+# Copyright 2022 RDK Management
+# Licensed under the Apache License, Version 2.0
+##########################################################################
+
+echo "$DB_URL"
+echo "$DB_USERNAME"
+echo "DOCKER_IMAGE_NAME"
+
+sed -i -e "s|jdbc:mysql:\/\/localhost:3306\/automatics?autoReconnect=true|${DB_URL}|" $CATALINA_HOME/AutomaticsConfig/hibernate.cfg.xml
+
+sed -i "s/<property name=\"hibernate.connection.username\">/&$DB_USERNAME/" $CATALINA_HOME/AutomaticsConfig/hibernate.cfg.xml
+
+password=`echo -n $DB_PASSWORD | base64`
+
+sed -i "s/<property name=\"hibernate.connection.password\">/&$password/" $CATALINA_HOME/AutomaticsConfig/hibernate.cfg.xml
+
+mkdir -p $CATALINA_HOME/logs/traces/
+
+chmod -R 777 $CATALINA_HOME/logs
+
+echo '' > $CATALINA_HOME/bin/mainUI.jmd
+
+echo '' > $CATALINA_HOME/bin/childUI.jmd
+
+echo '' > $CATALINA_HOME/mainUI.jmd
+
+echo '' > $CATALINA_HOME/childUI.jmd
+
+chmod 777 $CATALINA_HOME/childUI.jmd $CATALINA_HOME/mainUI.jmd $CATALINA_HOME/bin/childUI.jmd $CATALINA_HOME/bin/mainUI.jmd
+
+sh $CATALINA_HOME/bin/catalina.sh run
+
+sleep 140
+
+chmod -R 777 $CATALINA_HOME/logs
