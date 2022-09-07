4b974f983b4e1a31dd0c45443d3230082650e912
sshere654 <Shameem_Shereef@comcast.com>
2022-09-07 12:07:54 +0530

    AUTOMATICS-175 : Automatics Jenkins server and Job creation automation
    Reason for change:  Automatics Jenkins server and Job creation automation
    Test Procedure: Not applicable.
    Risks: None
    
    Change-Id: Ied552daaddb54d2c0e6f9f85b8b1986b8ebd846a
    Signed-off-by: Shameem M Shereef <shameemm.s@tataelxsi.co.in>

---
+++ automationScripts/build/Automatics-Props/editConfig.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2022 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+echo "$UI_USERNAME"
+echo "$AUTOMATICS_PROPS_URL"
+echo "$AUTOMATICS_ORCH_URL"
+echo "$DEVICE_MANAGER_URL"
+
+sed -i "s/user=/&$UI_USERNAME/" $CATALINA_HOME/webapps/automatics/config.properties
+
+sed -i "s/pwd=/&$UI_PASSWORD/" $CATALINA_HOME/webapps/automatics/config.properties
+
+#sed -i "s/device.props=/&\$CATALINA_HOME\/webapps\/automatics\/device_config.json/" $CATALINA_HOME/webapps/automatics/automatics.properties
+
+#sed -i "s/automatics.url=/&\$AUTOMATICS_ORCH_URL/" $CATALINA_HOME/webapps/automatics/automatics.properties
+
+#sed -i "s/DEVICE_MANAGER_BASE_URL=/&\$DEVICE_MANAGER_URL/" $CATALINA_HOME/webapps/automatics/automatics.properties
+
+mkdir -p $CATALINA_HOME/logs/traces/
+
+chmod -R 777 $CATALINA_HOME/logs
+
+sh $CATALINA_HOME/bin/catalina.sh run
+
+sleep 140
+
+chmod -R 777 $CATALINA_HOME/logs
---
+++ jenkinsServerSetup/install_jenkins_server.sh
@@ -0,0 +1,98 @@
+# /bin/bash
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2022 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+#Checking whether docker is already installed in the VM
+if [[ $(which docker) && $(docker --version) ]]; then
+    echo "Update docker"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+  else
+    echo "Install docker"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+fi
+
+echo -e "\n\n<-------------Docker Installtion Path-------------->"
+which docker
+echo -e "\n\n<-------------Docker Version-------------->"
+docker --version
+
+# Getting user name to add in the docker user group so that
+# sudo command is not required for this corresponding user to 
+# execute docker commands
+echo -e "\n\nEnter the VM user who require the premission to run docker commands without sudo : "
+read user
+
+# creating new User group "docker" if there is an input received from user
+if [ -z "$user" ]
+then
+      echo -e "\033[0;31mNo input received from \$user !!! Skipping docker group creation and user addition\033[0m"
+else
+	if [ $(getent group docker) ]; then
+	  echo "Going to add ${user} to the user group Docker"
+	  usermod -aG docker ${user}
+	  echo "Added ${user} to the user group"
+	  #newgrp docker
+      echo -e "Successfully created usergroup docker and added $user to it !! \n\033[0;31mRestart of the VM is needed for this change to take effect\033[0m"
+    else
+	  groupadd docker
+	  usermod -aG docker ${user}
+	  #newgrp docker
+      echo -e "Successfully created usergroup docker and added $user to it !! \n\033[0;31mRestart of the VM is needed for this change to take effect\033[0m"
+    fi
+fi
+
+#Configuring Docker to start on boot
+systemctl enable docker.service
+systemctl enable containerd.service
+
+#Getting jenkins docker image from docker hub and building custom Jenkins docker image for automatics
+docker build -f ./jenkinsDockerFile -t automatics_jenkins:jdk8 .
+
+echo -e "\n\nSuccesfully built Automatics Jenkins Docker image"
+
+mkdir -p /mnt/automatics/maven/maven-repo
+mkdir -p /mnt/automatics/jenkins_home
+
+chmod -R 777 /mnt/automatics
+
+echo -e "\n\nEnter the port in which Jenkins server to be started : "
+read port
+#Starting the jenkins docker conatiner
+docker run --name automatics_jenkins --restart=on-failure --detach \
+  --network jenkins --publish ${port}:8080 --publish 50000:50000 \
+  --volume /mnt/automatics/jenkins_home:/var/jenkins_home \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  #--volume /mnt/automatics/maven:/usr/share/maven
+  automatics_jenkins:jdk8
+
+echo -e "\n\nSuccesfully Started Automatics Jenkins Docker Conatiner"
+echo -e "Jenkins server is started in port \033[0;31m${port}\033[0m"
+
+#Creating the required jobs in jenkins
+docker cp jobs/. automatics_jenkins:/var/jenkins_home/jobs/
+
+docker restart automatics_jenkins
+
+#echo -e "\n\nMaven home directory seen in VM : \033[0;31m/mnt/automatics/maven\033[0m"
+echo -e "\nMaven home directory inside DOCKER conatiner : \033[0;31m/usr/share/maven\033[0m"
+echo -e "\nJenkins home location : \033[0;31m/mnt/automatics/jenkins_home\033[0m"
+
